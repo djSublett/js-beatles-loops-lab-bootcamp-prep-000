8af92fe5894bf2ed27403fcdd432dec1cb04f260
Dale Sublett <djsublett04@gmail.com>
2019-03-28 01:10:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ function theBeatlesPlay(musicians, instruments) {
   var beatle = [];
   for (i = 0; i < musicians.length; i++){
     
-      musicians[0] = "";
-      instruments[0] = "John Lennon plays guitar."
-      
-    
   }
 }
