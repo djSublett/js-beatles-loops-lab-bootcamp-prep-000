39fe23a51bf74d9c921ae87e985cfbec1498def8
Dale Sublett <djsublett04@gmail.com>
2019-03-28 01:11:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var beatle = [];
   for (var i = 0; i < musicians.length; i++){
-    array.push(musicians[i])
+    array.push(musicians[i] + ' plays ' + instruments[i]);
   }
 }
